5b7c4190f4935f31fdf978f0b8830f4cea186327
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-07-19 20:20:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 function setup() {
   canvas = createCanvas(300, 300);
-  canvas.center();
+  canvas.position(600,320);
   video = createCapture(VIDEO);
   video.hide();
+  Classifier = ml5.imageClassifier("Mobilenet",modelLoaded);
 }
-
-
+function modelLoaded(){
+  console.log("Dummy");
+}
+function draw(){
+  image(video,0,0,300,300);
+  Classifier.classify(video,gotResult);
+}
+function gotResult(error,result){
+  if(error){
+    console.error(error);
+  }else{
+    console.log(result);
+    document.getElementById("ObjectName").innerHTML = result[0].label;
+    document.getElementById('Accuracy').innerHTML = result[0].confidence.toFixed(5) * 100 + "%";
+  }
+}
 
